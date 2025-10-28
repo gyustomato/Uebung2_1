16909027b3485a9289dfbc4350e332a22426f647
gyustomato <ddurmaz@smail.uni-koeln.de>
2025-10-28 10:54:26 +0100

    P2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,13 @@
+package p2;
+
+public class P2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 18;
+		boolean b = false;
+		if (i > 10) {
+			b = true;
+		}
+	}
+}
